5f11d617ed9b18a1ba66fe0d2778dae65b94c9a3
钊林 <teachcourse@sohu.com>
2017-04-23 14:52:13 +0800

    将WatchView改写为经典的Builder设计模式

--- app/src/main/java/cn/teachcourse/view/WatchViewActivity.java
+++ app/src/main/java/cn/teachcourse/view/WatchViewActivity.java
@@ -4,25 +4,17 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.util.Calendar;
-
-import cn.teachcourse.R;
-import cn.teachcourse.utils.LogUtils;
-
 
 public class WatchViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WatchView.Builder builder=new WatchView.Builder(this);
-        builder.setRadius(300f);
-//        builder.setMinuteColor(Color.BLUE);
-//        builder.setSecondColor(Color.RED);
-//        builder.setHourColor(0xff999999);
+        Builder builder=new WatchViewImpl.BuilderImpl(this);
+        builder.setRadius(300f).setMinuteColor(Color.BLUE).setSecondColor(Color.RED).setHourColor(0xff999999);
 //        builder.setPadding(10f);
-        builder.setShortScaleColor(Color.WHITE);
-        WatchView watchView=builder.create();
+//        builder.setShortScaleColor(Color.WHITE);
+        WatchViewImpl watchView=builder.create();
         setContentView(watchView);
 //        setContentView(R.layout.activity_watch_view);
     }
