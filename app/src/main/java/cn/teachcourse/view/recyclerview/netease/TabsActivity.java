ec210e69ff9917af1ae6e29616a8b014acf4a249
Administrator <123456>
2018-04-13 16:44:04 +0800

    add RecyclerCarryOnItemClick class

--- app/src/main/java/cn/teachcourse/view/recyclerview/netease/TabsActivity.java
+++ app/src/main/java/cn/teachcourse/view/recyclerview/netease/TabsActivity.java
@@ -8,104 +8,112 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.AttributeSet;
-import android.util.Xml;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.xmlpull.v1.XmlPullParser;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import cn.teachcourse.R;
+import cn.teachcourse.view.recyclerview.RecyclerCarryOnItemClick;
 
 public class TabsActivity extends AppCompatActivity {
-    private static final String TAG=TabsActivity.class.getName();
-    private static final String toItems[]={"头条","财经","热点","政务","直播","社会","娱乐","健康","历史","军事"};
-    private static final String fromItems[]={"博客","彩票","段子","轻松一刻","房产","论坛","时尚","体育","移动互联"};
-    private RecyclerView fromRecyclerView,toRecyclerView;
-    List<String> mList=new ArrayList<String>();
-    List<String> list=new ArrayList<String>();
-    private RecyclerAdapter mAdapter;
-    public static void start(Context context){
-        start(context,null);
+    private static final String TAG = TabsActivity.class.getName();
+    private static final String toItems[] = {"头条", "财经", "热点", "政务", "直播", "社会", "娱乐", "健康", "历史", "军事"};
+    private static final String fromItems[] = {"博客", "彩票", "段子", "轻松一刻", "房产", "论坛", "时尚", "体育", "移动互联"};
+    private RecyclerCarryOnItemClick fromRecyclerView;
+    private RecyclerView toRecyclerView;
+    List<String> mFromList = new ArrayList<String>();
+    List<String> mToList = new ArrayList<String>();
+    private RecyclerAdapter mToAdapter;
+    private RecyclerAdapter mFromAdapter;
+
+    public static void start(Context context) {
+        start(context, null);
     }
-    public static void start(Context context,Intent extras){
-        Intent intent=new Intent(context,TabsActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        if(extras!=null){
+
+    public static void start(Context context, Intent extras) {
+        Intent intent = new Intent(context, TabsActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        if (extras != null) {
             intent.putExtras(intent);
         }
         context.startActivity(intent);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
         initView();
     }
+
     /**
      * 初始化布局控件
      */
-    private void initView(){
+    private void initView() {
 
-        GridLayoutManager gridLayoutManager=new GridLayoutManager(this,4);
-        fromRecyclerView=(RecyclerView)findViewById(R.id.from_recyclerview);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 4);
+        fromRecyclerView = (RecyclerCarryOnItemClick) findViewById(R.id.from_recyclerview);
         fromRecyclerView.setLayoutManager(gridLayoutManager);
-        for (String item:fromItems){
-            mList.add(item);
+        for (String item : fromItems) {
+            mFromList.add(item);
         }
-        fromRecyclerView.setAdapter(new RecyclerAdapter(mList));
+        mFromAdapter=new RecyclerAdapter(mFromList);
+        fromRecyclerView.setAdapter(mFromAdapter);
         fromRecyclerView.addItemDecoration(itemDecoration);
-        itemTouchHelper.attachToRecyclerView(fromRecyclerView);
+        fromRecyclerView.setOnItemClickListener(onItemClickListener);
 
-        GridLayoutManager gridLayoutManager2=new GridLayoutManager(this,4);
-        toRecyclerView=(RecyclerView)findViewById(R.id.to_recyclerview);
+        GridLayoutManager gridLayoutManager2 = new GridLayoutManager(this, 4);
+        toRecyclerView = (RecyclerView) findViewById(R.id.to_recyclerview);
         toRecyclerView.setLayoutManager(gridLayoutManager2);
-        for (String item:toItems){
-            list.add(item);
+        for (String item : toItems) {
+            mToList.add(item);
         }
-        mAdapter=new RecyclerAdapter(list);
-        toRecyclerView.setAdapter(mAdapter);
+        mToAdapter = new RecyclerAdapter(mToList);
+        toRecyclerView.setAdapter(mToAdapter);
         toRecyclerView.addItemDecoration(itemDecoration);
         itemTouchHelper.attachToRecyclerView(toRecyclerView);
 
     }
+
     /**
      * 重写RecyclerView.ItemDecoration
      */
-    private RecyclerView.ItemDecoration itemDecoration=new RecyclerView.ItemDecoration() {
+    private RecyclerView.ItemDecoration itemDecoration = new RecyclerView.ItemDecoration() {
         @Override
         public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
             super.getItemOffsets(outRect, view, parent, state);
-            outRect.left=15;
-            outRect.top=15;
-            outRect.right=15;
-            outRect.bottom=15;
+            outRect.left = 15;
+            outRect.top = 15;
+            outRect.right = 15;
+            outRect.bottom = 15;
         }
     };
+
     /**
      * 创建RecyclerView.ViewHolder
      */
-    private class OnItemViewHolder extends RecyclerView.ViewHolder{
+    private class OnItemViewHolder extends RecyclerView.ViewHolder {
         TextView textView;
+
         public OnItemViewHolder(View itemView) {
             super(itemView);
-            textView=(TextView)itemView.findViewById(R.id.text);
+            textView = (TextView) itemView.findViewById(R.id.text);
             textView.setBackgroundDrawable(getResources().getDrawable(R.drawable.bg_round_corner));
             textView.setGravity(Gravity.CENTER);
-            textView.setPadding(2,2,2,2);
+            textView.setPadding(2, 2, 2, 2);
         }
     }
+
     /**
      * 创建绑定数据的RecyclerView.Adapter
      */
-    private class RecyclerAdapter extends RecyclerView.Adapter<OnItemViewHolder>{
+    private class RecyclerAdapter extends RecyclerView.Adapter<OnItemViewHolder> {
         private List<String> list;
 
         public RecyclerAdapter(List<String> list) {
@@ -114,7 +122,7 @@ public class TabsActivity extends AppCompatActivity {
 
         @Override
         public OnItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view=((LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.list_item_text,parent,false);
+            View view = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.list_item_text, parent, false);
 
             return new OnItemViewHolder(view);
         }
@@ -130,10 +138,22 @@ public class TabsActivity extends AppCompatActivity {
             return list.size();
         }
     }
+
+    private RecyclerCarryOnItemClick.OnItemClickListener onItemClickListener=new RecyclerCarryOnItemClick.OnItemClickListener() {
+        @Override
+        public void onItemClick(RecyclerView.ViewHolder vh,int position) {
+               String item=mFromList.get(position);
+               mFromList.remove(item);
+               mFromAdapter.notifyDataSetChanged();
+               mToList.add(item);
+               mToAdapter.notifyDataSetChanged();
+        }
+    };
+
     /**
      * 创建ItemTouchHelper对象
      */
-    private ItemTouchHelper itemTouchHelper=new ItemTouchHelper(new ItemTouchHelper.Callback() {
+    private ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.Callback() {
         @Override
         public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
             final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN |
@@ -151,26 +171,26 @@ public class TabsActivity extends AppCompatActivity {
             if (viewHolder.getItemViewType() != target.getItemViewType()) {
                 return false;
             }
-            int fromPostion=viewHolder.getAdapterPosition();
-            int toPosition=target.getAdapterPosition();
-            if(fromPostion<toPosition){
-                for (int i=fromPostion;i<toPosition;i++){
-                    Collections.swap(list,i,i+1);
+            int fromPosition = viewHolder.getAdapterPosition();
+            int toPosition = target.getAdapterPosition();
+            if (fromPosition < toPosition) {
+                for (int i = fromPosition; i < toPosition; i++) {
+                    Collections.swap(mToList, i, i + 1);
                 }
-            }else{
-                for (int i=fromPostion;i>toPosition;i--){
-                    Collections.swap(list,i,i-1);
+            } else {
+                for (int i = fromPosition; i > toPosition; i--) {
+                    Collections.swap(mToList, i, i - 1);
                 }
             }
-            mAdapter.notifyDataSetChanged();
+            mToAdapter.notifyDataSetChanged();
             return true;
         }
 
         @Override
         public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-            int position=viewHolder.getAdapterPosition();
-            list.remove(position);
-            mAdapter.notifyDataSetChanged();
+            int position = viewHolder.getAdapterPosition();
+            mToList.remove(position);
+            mToAdapter.notifyDataSetChanged();
         }
     });
 }
